a941a1b7391dc7ef981f5dabe207083bf07ab8cf
Serenity112 <skuratov.prodamgarazh@gmail.com>
2024-03-14 01:01:24 +0300

    2я лаба (доп к 1й)

--- Lab1/DataGenerator.py
+++ Lab1/DataGenerator.py
@@ -17,23 +17,42 @@ def norm_dataset(mu, sigma, n):
         v1 = np.random.normal(mu1[i], sigma1[i], [n, 1])
         class1 = np.hstack((class1, v1))
 
+    y0 = np.zeros((n, 1), dtype=bool)
+    y1 = np.ones((n, 1), dtype=bool) 
+    
+    x = np.vstack((class0, class1))
+    y = np.vstack((y0, y1)).ravel()
+    
+    rng = np.random.default_rng()
+    arr = np.arange(2 * n) 
+    rng.shuffle(arr)
+    
+    x = x[arr]
+    y = y[arr]
 
-
-    return  class0, class1
+    return x, y, class0, class1
 
 
 def generate_points(N, ellipse_params, rectangle_params):
-    class_0_points = []
-    class_1_points = []
+    class0 = np.zeros((N, 2))
+    class1 = np.zeros((N, 2))
+    counter0 = 0
+    counter1 = 0
 
-    while len(class_0_points) < N or len(class_1_points) < N:
-        x = np.random.uniform(0, 100)
-        y = np.random.uniform(0, 100)
+    while counter0 < N or counter1 < N:
+        x = np.random.normal(loc=ellipse_params['center_x'], scale=ellipse_params['a']/3)
+        y = np.random.normal(loc=ellipse_params['center_y'], scale=ellipse_params['b']/3)
 
         # ßéöî
         if ((x - ellipse_params['center_x']) / ellipse_params['a'])**2 + ((y - ellipse_params['center_y']) / ellipse_params['b'])**2 <= 1:
-            class_0_points.append([x, y])
+            if counter0 < N:
+                class0[counter0, 0] = x
+                class0[counter0, 1] = y
+                counter0 += 1
             continue
+        
+        x = np.random.normal(loc=(rectangle_params['left'] + rectangle_params['right'])/2, scale=(rectangle_params['right'] - rectangle_params['left'])/6)
+        y = np.random.normal(loc=rectangle_params['bottom'], scale=(rectangle_params['top'] - rectangle_params['bottom'])/3)
 
         # Ðþìêà
         if (
@@ -41,6 +60,22 @@ def generate_points(N, ellipse_params, rectangle_params):
             rectangle_params['bottom'] < y < rectangle_params['top'] and
             ((x - rectangle_params['circle_center_x'])**2 + (y - rectangle_params['circle_center_y'])**2) > rectangle_params['circle_radius']**2
         ):
-            class_1_points.append([x, y])
+            if counter1 < N:
+               class1[counter1, 0] = x
+               class1[counter1, 1] = y
+               counter1 += 1
 
-    return np.array(class_0_points), np.array(class_1_points)
+    y0 = np.zeros((N, 1), dtype=bool)
+    y1 = np.ones((N, 1), dtype=bool) 
+    
+    x = np.vstack((class0, class1))
+    y = np.vstack((y0, y1)).ravel()
+    
+    rng = np.random.default_rng()
+    arr = np.arange(2 * N) 
+    rng.shuffle(arr)
+    
+    x = x[arr]
+    y = y[arr]
+
+    return x, y, class0, class1
--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,7 +1,62 @@
 import matplotlib.pyplot as plt
 import DataGenerator as dataGen
+import numpy as np
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 
+def draw_plt(data, indexes, title):
+    plt.hist(data[indexes, 0], bins='auto', alpha=0.7)
+    plt.hist(data[~indexes, 0], bins='auto', alpha=0.7)
+    plt.title(title)
+    plt.show()
+    
+def calculate_accurancy(pred, y):
+    return sum(pred == y) / len(y)
+
+def calculate_sensitivity_specificity(pred, y):
+    TP_test = sum((pred == 1) & (y == 1))
+    FN_test = sum((pred == 0) & (y == 1))
+    TN_test = sum((pred == 0) & (y == 0))
+    FP_test = sum((pred == 1) & (y == 0))
+
+    sensitivity = TP_test / (TP_test + FN_test)
+    specificity = TN_test / (TN_test + FP_test)
+    return sensitivity, specificity
+
+def predict(x, y, class0, class1):
+    _ = plt.scatter(class0[:, 0], class0[:, 1], marker=".", alpha=0.7)
+    _ = plt.scatter(class1[:, 0], class1[:, 1], marker=".", alpha=0.7)
+    plt.show()
+
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3, random_state=42)
+
+    Nvar = 17
+    clf = LogisticRegression(random_state=Nvar, solver='saga').fit(xtrain, ytrain)
+
+    Pred_test = clf.predict(xtest)
+    Pred_test_proba = clf.predict_proba(xtest)
+    title_test = "res1"
+    draw_plt(Pred_test_proba, ytest, title_test)
+
+    Pred_train = clf.predict(xtrain)
+    Pred_train_proba = clf.predict_proba(xtrain)
+    title_train = "res2"
+    draw_plt(Pred_train_proba, ytrain, title_train)
+    
+    acc_train = calculate_accurancy(Pred_train, ytrain)
+    acc_test = calculate_accurancy(Pred_test, ytest) 
+    print("acc_train = ", acc_train, ", acc_test = ", acc_test)
+    
+    sensitivity_test, specificity_test = calculate_sensitivity_specificity(Pred_test, ytest)
+    print("Sensitivity (Test):", sensitivity_test)
+    print("Specificity (Test):", specificity_test)
+
+    sensitivity_train, specificity_train = calculate_sensitivity_specificity(Pred_train, ytrain)
+    print("Sensitivity (Train):", sensitivity_train)
+    print("Specificity (Train):", specificity_train)
+    
 def task1():
     mu0 = [0, 2, 3]
     mu1 = [3, 5, 1]
@@ -11,31 +66,18 @@ def task1():
     sigma = [sigma0, sigma1]
     col = len(mu0)
     n = 1000
-
-    class0, class1 = dataGen.norm_dataset(mu, sigma, n)
-
-    for i in range(0, col):
-        _ = plt.hist(class0[:, i], bins='auto', alpha=0.7)
-        _ = plt.hist(class1[:, i], bins='auto', alpha=0.7)
-        plt.savefig('output/' + str(i + 1) + '.png')
-
-    plt.clf()
-
-    _ = plt.scatter(class0[:, 0], class0[:, 2], marker=".", alpha=0.7)
-    _ = plt.scatter(class1[:, 0], class1[:, 2], marker=".", alpha=0.7)
-    plt.savefig('output/scatter.png')
-    plt.show()
+    
+    x, y, class0, class1 = dataGen.norm_dataset(mu, sigma, n)
+    predict(x, y, class0, class1)
+    
 
 def task2():
-    ellipse_params = {'center_x': 50, 'center_y': 50, 'a': 30, 'b': 40}
-    rectangle_params = {'left': 0, 'right': 100, 'bottom': 0, 'top': 50, 'circle_center_x': 50, 'circle_center_y': 50, 'circle_radius': 8}
-    N = 500
+    ellipse_params = {'center_x': 50, 'center_y': 50, 'a': 25, 'b': 25}
+    rectangle_params = {'left': 0, 'right': 100, 'bottom': 0, 'top': 50, 'circle_center_x': 50, 'circle_center_y': 50, 'circle_radius': 30}
+    N = 1000
     
-    class_0_points, class_1_points = dataGen.generate_points(N, ellipse_params, rectangle_params)
-    _ = plt.scatter(class_0_points[:, 0], class_0_points[:, 1], marker=".", alpha=0.7)
-    _ = plt.scatter(class_1_points[:, 0], class_1_points[:, 1], marker=".", alpha=0.7)
-    plt.show()
-
+    x, y, class0, class1 = dataGen.generate_points(N, ellipse_params, rectangle_params)
+    predict(x, y, class0, class1)
 
 if __name__ == "__main__":
     task2()
