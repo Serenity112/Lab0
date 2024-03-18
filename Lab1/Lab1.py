deff6fc34c1c982ff20c7fff5d8bf30d43857ea4
Serenity112 <skuratov.prodamgarazh@gmail.com>
2024-03-18 09:36:00 +0300

    lab 3

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import DataGenerator as dataGen
 import numpy as np
+import scikitplot as skplt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve, auc
 
 def draw_plt(data, indexes, title):
     plt.hist(data[indexes, 0], bins='auto', alpha=0.7)
@@ -70,14 +76,69 @@ def task1():
     x, y, class0, class1 = dataGen.norm_dataset(mu, sigma, n)
     predict(x, y, class0, class1)
     
-
 def task2():
-    ellipse_params = {'center_x': 50, 'center_y': 50, 'a': 25, 'b': 25}
-    rectangle_params = {'left': 0, 'right': 100, 'bottom': 0, 'top': 50, 'circle_center_x': 50, 'circle_center_y': 50, 'circle_radius': 30}
+    ellipse_params = {'center_x': 50, 'center_y': 50, 'a': 45, 'b': 45}
+    rectangle_params = {'left': 0, 'right': 100, 'bottom': 0, 'top': 70, 'circle_center_x': 50, 'circle_center_y': 50, 'circle_radius': 10}
     N = 1000
     
     x, y, class0, class1 = dataGen.generate_points(N, ellipse_params, rectangle_params)
     predict(x, y, class0, class1)
 
+def classify(classifier, xtrain, xtest, ytrain, ytest):
+
+    print("Train accuracy = ", classifier.score(xtrain, ytrain))
+    print("Test accuracy = ", classifier.score(xtest, ytest))
+    sensitivity_test, specificity_test = calculate_sensitivity_specificity(classifier.predict(xtest), ytest)
+    print("Test sensitivity = ", sensitivity_test)
+    print("Test specificity = ", specificity_test)
+    
+    y_probas_tree = classifier.predict_proba(xtest)
+    skplt.metrics.plot_roc(ytest, y_probas_tree, figsize = (10,10))
+    plt.show()
+
+    AUC = roc_auc_score(ytest, y_probas_tree[:,1])
+    print("AUC:"+str(AUC))
+
+def lab3():
+    ellipse_params = {'center_x': 50, 'center_y': 50, 'a': 45, 'b': 45}
+    rectangle_params = {'left': 0, 'right': 100, 'bottom': 0, 'top': 70, 'circle_center_x': 50, 'circle_center_y': 50, 'circle_radius': 10}
+    N = 1000
+    x, y, class0, class1 = dataGen.generate_points(N, ellipse_params, rectangle_params)
+
+    plt.scatter(class0[:, 0], class0[:, 1], marker=".", alpha=0.7, label='1')
+    plt.scatter(class1[:, 0], class1[:, 1], marker=".", alpha=0.7, label='2')
+    plt.show()
+
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3)
+
+
+    print("Tree")
+    classifier = DecisionTreeClassifier(max_depth=5)  
+    classifier.fit(xtrain, ytrain)
+    classify(classifier, xtrain, xtest, ytrain, ytest)
+
+    print("Forest")
+    classifier = RandomForestClassifier(max_depth=5)  
+    classifier.fit(xtrain, ytrain)
+    classify(classifier, xtrain, xtest, ytrain, ytest)
+
+    # FOR loop
+    best_auc = 0
+    best_num_trees = 0
+    num_trees_range = range(1, 301, 10)
+    
+    for num_trees in num_trees_range:
+        forest = RandomForestClassifier(n_estimators=num_trees, random_state=0)
+        forest.fit(xtrain, ytrain)
+        y_probas = forest.predict_proba(xtest)
+        auc = roc_auc_score(ytest, y_probas[:, 1])
+        if auc > best_auc:
+            best_auc = auc
+            best_num_trees = num_trees
+
+    print("Best AUC:", best_auc)
+    print("Optimal tree num:", best_num_trees)
+
+
 if __name__ == "__main__":
-    task2()
+    lab3()
